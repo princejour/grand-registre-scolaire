830b1bf81bf8ed5504c3782dab04e53f52eb7807
princejour <61734521+princejour@users.noreply.github.com>
2026-06-24 18:40:12 +0100

    إفراغ الذاكرة القديمة بعد تحديث التطبيق

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-const CACHE='grand-registre-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let y=x.clone();caches.open(CACHE).then(c=>c.put(e.request,y)).catch(()=>{});return x}).catch(()=>caches.match('./index.html'))))});
+const CACHE='grand-registre-pages-v3';
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))));
+  self.clients.claim();
+});
